1907be2dca32d2621374dc4a3234a116d21ed89a
marihachi <marihachi0620@gmail.com>
2023-11-24 22:28:43 +0900

    update build

--- script/build.js
+++ script/build.js
@@ -2,6 +2,6 @@ import { execSync } from 'child_process';
 
 execSync('mkdir -p dist');
 execSync('cp ./src/carol.d.ts ./dist/carol.d.ts');
-execSync('npx uglifyjs -c -m --module --keep-fargs --keep-fnames --comments /^!/ ./src/carol.js -o ./dist/carol.js');
+execSync('npx uglifyjs ./src/carol.js -o ./dist/carol.js -c -m --keep-fargs --keep-fnames --comments /^!/');
 
 console.log('Done');
