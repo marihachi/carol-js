62e8fe5168ef7e1978a54d441e2d2c315920fb87
marihachi <marihachi0620@gmail.com>
2025-05-30 01:24:49 +0900

    Update examples

--- examples/uuid/index.js
+++ examples/uuid/index.js
@@ -1,8 +1,8 @@
-import carol, { seq } from 'carol-js';
+import carol from 'carol-js';
 import assert from 'node:assert';
 
 const hex = carol(/[0-9a-f][0-9a-f]/);
-const uuid = seq([
+const uuid = carol.seq([
   hex.many({ length: 4 }),
   carol(/-/),
   hex.many({ length: 2 }),
@@ -13,10 +13,13 @@ const uuid = seq([
   carol(/-/),
   hex.many({ length: 6 }),
 ]);
-const regex = uuid.toRegex("i");
+const regex = uuid.toRegex({ flags: "i", exact: true });
 
 console.log(`regex: ${regex}`);
 
-assert.strictEqual(regex.test("550e8400-e29b-41d4-a716-446655440000"), true);
-assert.strictEqual(regex.test("550e8400-e29be29b-41d4-a716-446655440000"), false);
+assert.strictEqual(
+  regex.source,
+  "^(?:[0-9a-f][0-9a-f]){4}-(?:[0-9a-f][0-9a-f]){2}-(?:[0-9a-f][0-9a-f]){2}-(?:[0-9a-f][0-9a-f]){2}-(?:[0-9a-f][0-9a-f]){6}$"
+);
+assert.strictEqual(regex.flags, "i");
 console.log("test ok");
